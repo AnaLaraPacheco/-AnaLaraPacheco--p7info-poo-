fd89997228d95f2fa5158262dbbd6b4026c950a1
AnaLaraPacheco <100225863+AnaLaraPacheco@users.noreply.github.com>
2022-06-20 17:39:51 -0300

    Update nota_fiscal.ts

--- avaliacoes/avaliação-04/nota_fiscal.ts
+++ avaliacoes/avaliação-04/nota_fiscal.ts
@@ -12,7 +12,6 @@ export class NotaFiscal {
     data: Date;
     cliente: Cliente;
     items :  Array<ItemNotaFiscal>;  
-    valorNota : number;
 
     /* Métodos */
 
@@ -22,10 +21,7 @@ export class NotaFiscal {
         this.codigo = codigo;
         this.data = new Date(); 
         this.cliente = cliente; 
-        this.valorNota =0.0;
         this.items = new Array<ItemNotaFiscal>();
-
-
     }
 
     /* Métodos Acessores */
@@ -50,29 +46,53 @@ export class NotaFiscal {
         this.data = data;
     }
 
-     
-
     adicionarItem(item: ItemNotaFiscal) {
         this.items.push(item);
-        
-        this.valorNota += item.valor;
     }
 
     // Percorrer o array items e calcular o valor total da NotaFiscal
     calcularValorNotaFiscal() : number {
-        let valorCalculado = 0;
+        let valorNota = 0;
+        
+        for (let i = 0; i < this.items.length; i++) {
+            valorNota += this.items[i].valor;
+        }
 
-
-        return valorCalculado;
-
+        return valorNota;
     }
 
     // Imprimir a NotaFiscal conforme o Layout definido
-    
     imprimirNotaFiscal(): void {
+        let date = new Date();
+        let dia = date.getDate().toString();
+        let diaF = (dia.length == 1) ? '0' + dia : dia;
+        let mes = (date.getMonth()+1).toString();
+        let mesF = (mes.length == 1) ? '0' + mes : mes;
 
-
+        console.log("-------------------------------------------------------------------------------------------");
+        console.log('NOTA FISCAL ' + '(' + diaF + '/' + mesF + '/' + date.getFullYear() + ')');
+        console.log('ID do cliente: ' + this.cliente.getid());
+        console.log('Nome do cliente: ' + this.cliente.getnome());
+        console.log('CPF/CNPJ: ' + this.cliente.getcnpjcpf());
+        console.log("-------------------------------------------------------------------------------------------");
+        console.log("ITENS");
+        console.log("-------------------------------------------------------------------------------------------");
+        console.log('Seq\tDescrição\t\t\tQTD\tValor Unit\t\tPreço');
+        console.log('---\t---------\t\t\t---\t----------\t\t-----');
+        
+        for (let i = 0; i < this.items.length; i++) {
+            console.log(
+                this.items[i].id +'\t' +
+                this.items[i].produto.getdescricao() + '\t\t\t' +
+                this.items[i].quantidade + '\t' +
+                this.items[i].valor.toFixed(2) + '\t\t\t' +
+                (this.items[i].valor * this.items[i].quantidade).toFixed(2)
+            )
+        }
+        
+        console.log("___________________________________________________________________________________________");
+        console.log('Valor Total: ' + this.calcularValorNotaFiscal());
+        
+        
     }
-
-    
 }
